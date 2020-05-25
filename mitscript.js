0d75035a2faa094ebbc7fb7487c6d06bfdd02a21
MartinMundelingAndersen <56107508+MartinMundelingAndersen@users.noreply.github.com>
2020-05-25 20:31:07 +0200

    version 7 - 250520

--- mitscript.js
+++ mitscript.js
@@ -1,1 +1,10 @@
 "use strict";
+
+const antal = document.getElementById("antal");
+document.getElementById("op").addEventListener("click", function () {
+    antal.value = parseInt(antal.value) + 1;
+})
+
+document.getElementById("ned").addEventListener("click", function () {
+    antal.value = parseInt(antal.value) - 1;
+})
