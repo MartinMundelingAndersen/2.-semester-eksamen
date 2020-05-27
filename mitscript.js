83787fe82e0618a17b4055d0ef195faf301fac5b
MartinMundelingAndersen <56107508+MartinMundelingAndersen@users.noreply.github.com>
2020-05-28 00:06:24 +0200

    version 18 - 270520

--- mitscript.js
+++ mitscript.js
@@ -1,5 +1,18 @@
 "use strict";
 
+//video - starter/pauser intro-videoen når man scroller
+const AVIdListe = ["intro"];
+const AVIndholdsliste = [];
+
+AVIndholdsliste[0] = document.getElementById("intro")
+
+window.addEventListener("scroll", function () {
+    aktiverMultimedier();
+});
+
+
+
+// Tilføher værdi når man
 const antal = document.getElementById("antal");
 document.getElementById("op").addEventListener("click", function () {
     antal.value = parseInt(antal.value) + 1;
