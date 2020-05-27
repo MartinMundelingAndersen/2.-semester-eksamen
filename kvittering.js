1951d543bdaff0de43aaf180de4c57e9d8c4d9a2
andersdem <56107522+andersdem@users.noreply.github.com>
2020-05-27 14:44:00 +0200

    Version 15 - 27-05-2020

--- kvittering.js
+++ kvittering.js
@@ -4,6 +4,6 @@ const kt = document.getElementById("kvittering");
 
 kt.insertAdjacentHTML("beforeend", "<h4>Oplysninger: </h4>" + sessionStorage.getItem("navn") + "<br>");
 kt.insertAdjacentHTML("beforeend", sessionStorage.getItem("adresse") + "<br>");
-kt.insertAdjacentHTML("beforeend", sessionStorage.getItem("pogb") + "<br>");
+kt.insertAdjacentHTML("beforeend", sessionStorage.getItem("postnrogby") + "<br>");
 kt.insertAdjacentHTML("beforeend", "<h4>Levering: </h4>" + sessionStorage.getItem("levering") + "<br>");
 kt.insertAdjacentHTML("beforeend", "<h4>Betaling: </h4>" + sessionStorage.getItem("betaling") + "<br>");
